c3ec6e95be02ea5f165139234bb23c90d42bcc3d
Mohamed Beshri Amer <mohamed.beshri.amer@gmail.com>
2024-09-07 01:01:58 +0300

    finish

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import {Routes, Route} from "react-router-dom";
 import Home from "./pages/Home";
 import Serial from "./pages/Serial";
+import BrowserRouter from 'react-router-dom/BrowserRouter'
 function App() {
   return (
    <>
-   <Routes>
+   <Routes basename={process.env.PUBLIC_URL}>
       <Route path="/" element={<Home/>} /> 
       <Route path="/serial" element={<Serial/>} />
    </Routes>
